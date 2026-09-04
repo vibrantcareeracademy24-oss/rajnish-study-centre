45a2cbf9ed6ae109d14e468b342b76cccdc049a3
vibrantcareeracademy24-oss <vibrantcareeracademy24@gmail.com>
2026-09-04 13:28:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// Supabase Free Database settings.
-// Paste ONLY your Supabase Project URL and anon/public key here.
-const SUPABASE_URL = "NEXT_PUBLIC_SUPABASE_URL=https://yetqjgiqhgelcyiqmnuk.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_dpujKe1m59ux5qghCy91vA_7EWt0S3v";
-const SUPABASE_ANON_KEY = "NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_dpujKe1m59ux5qghCy91vA_7EWt0S3v";
+// Supabase Free Database settings
+const SUPABASE_URL = "https://kathxxseycwncamlciab.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_efx4xAa5t8wZfDjstLGQ0Q_WIRIjJtQ";
